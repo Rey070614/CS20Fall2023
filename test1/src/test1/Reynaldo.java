93421b59b1e894e5d2f310c94423c16ce65098df
27165005 <27165005@L815WD220003.edu.cbe.ab.ca>
2023-09-12 09:43:51 -0600

    9:44

---
+++ test1/src/test1/Reynaldo.java
@@ -0,0 +1,17 @@
+package test1;
+
+public class Reynaldo {
+
+	public static void main(String[] args) 
+	{
+		//Declaration
+		int age = 16;
+		double cost = 16.50;
+		String name = "jeff";
+		char Letter = 'f';
+		
+		System.out.print(name+" costs "+(age+cost));
+		//System.out.print("comp 20");
+	}
+
+}
